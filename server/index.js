15c760815614ea2eb7dcab58797d85bb7d529926
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-03-23 15:59:40 +0000

    Backend folder structure updated

--- server/index.js
+++ server/index.js
@@ -2,14 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js"
+
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://abayomibalogun:Oluwatobi1@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://abayomibalogun:labalaba@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
