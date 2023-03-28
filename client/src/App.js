e27987fc82d67618ace33ca384e45c9f7a391ff5
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-03-28 18:54:51 +0100

    redux done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import useStyles from "./styles";
+import { useDispatch } from 'react-redux';
+import { getPosts } from "./actions/posts"
 
 
 
@@ -11,6 +13,12 @@ import useStyles from "./styles";
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
 
     return (
         <Container maxWidth="lg">
--- server/index.js
+++ server/index.js
@@ -7,17 +7,18 @@ import postRoutes from "./routes/posts.js"
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://abayomibalogun:labalaba@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority"
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL = "mongodb+srv://abayomibalogun:labalaba@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority" 
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(error.message))
 
-// mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false); 
