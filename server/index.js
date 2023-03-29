d12fa005a1ff39089f0e0be5aee6a9d2cac44337
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-03-29 17:18:35 +0100

    updated the server side and fixed the bug causing network error

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 
-const CONNECTION_URL = "mongodb+srv://abayomibalogun:labalaba@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority" 
+const CONNECTION_URL = "mongodb+srv://abayomibalogun:labalaba@cluster0.1y6paaw.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000
-
+ 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
