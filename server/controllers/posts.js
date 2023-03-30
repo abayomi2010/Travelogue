375e2c4343e6947cf32ef550b1b0ab22b9336d79
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-03-30 10:31:21 +0100

    minor fixes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -8,8 +8,6 @@ const router = express.Router();
 export const getPosts = async (req, res) => { 
     try {
         const postMessages = await PostMessage.find();
-        console.log(postMessages); 
-        console.log("a");   
         res.status(200).json(postMessages);
         
     } catch (error) {
